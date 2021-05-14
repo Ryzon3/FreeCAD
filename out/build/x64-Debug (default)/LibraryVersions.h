3630a9e03ae68539fd4c71ea841d34bd5654aade
Ryzon3 <ryanmcole20@gmail.com>
2021-05-14 13:32:38 -0400

    Revert "Revert "MakaCAD Branding Changes""
    This reverts commit 3cf3fc3af2800b7dce8e8230a52c482da79f3ab9.

---
+++ out/build/x64-Debug (default)/LibraryVersions.h
@@ -0,0 +1,13 @@
+#ifndef LIBRARY_VERSIONS_H
+#define LIBRARY_VERSIONS_H
+
+/* LibraryVersions.h.  Generated from LibraryVersions.h.cmake by cmake.  */
+
+/* #undef HAVE_OCC_VERSION */
+
+#if defined(HAVE_OCC_VERSION)
+#  include </Standard_Version.hxx>
+#endif
+
+#endif // LIBRARY_VERSIONS_H
+
